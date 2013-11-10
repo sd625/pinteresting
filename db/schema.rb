f81c96980473b83e83be22534f5a356b3d5b917f
Shray Chandra <shraychandra@outlook.com>
2013-11-10 13:42:44 -0500

    made pin changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131109213357) do
+ActiveRecord::Schema.define(version: 20131110173300) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
